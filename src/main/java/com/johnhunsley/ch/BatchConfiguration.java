03776cc2b3be59c7683a0931e328c6745f472ffc
U0181792 <john.hunsley@thomsonreuters.com>
2018-09-03 15:08:16 +0100

    write json and csv

--- src/main/java/com/johnhunsley/ch/BatchConfiguration.java
+++ src/main/java/com/johnhunsley/ch/BatchConfiguration.java
@@ -3,11 +3,13 @@ package com.johnhunsley.ch;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -16,26 +18,32 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.LineTokenizer;
+import org.springframework.batch.item.file.transform.*;
+import org.springframework.batch.item.support.CompositeItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+
+import java.util.ArrayList;
+import java.util.IllegalFormatException;
 import java.util.Iterator;
+import java.util.List;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
 
-    @Value("${aws.access.key}")
+    @Value("${aws.accessKeyId}")
     private String awsAccessKey;
 
-    @Value("${aws.secret.key}")
+    @Value("${aws.secretKey}")
     private String awsSecretKey;
 
     @Value("${s3.bucket.name}")
@@ -43,6 +51,12 @@ public class BatchConfiguration {
 
     @Value("${s3.key.name}")
     private String objectKey;
+
+    @Value("${json.output.file.path}")
+    private String jsonOutputFilePath;
+
+    @Value("${csv.output.file.path}")
+    private String csvOutputFilePath;
 
     @Autowired
     private AmazonS3 amazonS3;
@@ -82,25 +96,70 @@ public class BatchConfiguration {
 
     @Bean
     public AmazonS3 buildClient() {
-        AWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-        AmazonS3 s3client = AmazonS3ClientBuilder
+
+        if(awsAccessKey == null || awsSecretKey == null) {
+            return AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withCredentials(InstanceProfileCredentialsProvider.getInstance())
                 .withRegion(Regions.EU_WEST_1)
                 .build();
-        return s3client;
+
+        } else {
+            AWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+            return AmazonS3ClientBuilder
+                    .standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                    .withRegion(Regions.EU_WEST_1)
+                    .build();
+        }
     }
 
     @Bean
-    public ItemWriter writer() {
-        return list -> {
-            Iterator itr = list.iterator();
-
-            while(itr.hasNext()) {
-                System.out.println(mapper().writeValueAsString(itr.next()));
-            }
-        };
+    public CompositeItemWriter<Company> compositeItemWriter() {
+        CompositeItemWriter<Company> writer = new CompositeItemWriter<>();
+        List<ItemWriter<? super Company>> writers = new ArrayList<>();
+        writers.add(jsonFileItemWriter());
+        writers.add(csvFileItemWriter());
+        writer.setDelegates(writers);
+        return writer;
     }
+
+    private FlatFileItemWriter<Company> jsonFileItemWriter() {
+        FlatFileItemWriter<Company> writer = new FlatFileItemWriter<>();
+        writer.setResource(new FileSystemResource(jsonOutputFilePath));
+        writer.setLineAggregator(new DelimitedLineAggregator<Company>() {
+            @Override
+            public String aggregate(Company company) {
+                try {
+                    return mapper().writeValueAsString(company);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                    throw new IllegalArgumentException(e);
+                }
+            }
+
+            @Override
+            public void setDelimiter(String delimiter) {
+                super.setDelimiter("\\n");
+            }
+        });
+
+        return writer;
+    }
+
+    private FlatFileItemWriter<Company> csvFileItemWriter() {
+        FlatFileItemWriter<Company> writer = new FlatFileItemWriter<>();
+        writer.setResource(new FileSystemResource(csvOutputFilePath));
+        DelimitedLineAggregator<Company> aggregator = new DelimitedLineAggregator<>();
+        aggregator.setDelimiter(",");
+        BeanWrapperFieldExtractor extractor = new BeanWrapperFieldExtractor();
+        extractor.setNames(new String[] { "companyNumber", "companyName"});
+        aggregator.setFieldExtractor(extractor);
+        writer.setLineAggregator(aggregator);
+        return writer;
+    }
+
+
 
     @Bean
     public Step step1(ItemWriter writer) {
@@ -111,7 +170,7 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Job importFromS3AndPrintJob(Step step1) {
+    public Job importFromS3AndWriteJob(Step step1) {
         return jobBuilderFactory.get("importS3Job")
                 .listener(new JobExecutionListenerSupport() {
                     @Override
